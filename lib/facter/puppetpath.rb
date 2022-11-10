5e0992bf7248deecb45aaf5a5463baa86a5a1c2d
Grace Lee <62140283+golflimaechoecho@users.noreply.github.com>
2022-11-10 12:06:56 +1100

    improve syntax for custom fact

--- lib/facter/puppetpath.rb
+++ lib/facter/puppetpath.rb
@@ -1,15 +1,6 @@
 # example custom fact
 Facter.add(:puppetpath) do
-  confine :kernel do |value|
-    value == 'Linux'
+  setcode do
+    Facter::Core::Execution.which('puppet')
   end
-  setcode 'which puppet'
-end
-
-Facter.add(:puppetpath) do
-  confine :osfamily do |value|
-    value == 'Windows'
-  end
-  # Windows uses 'where' instead of 'which'
-  setcode 'where puppet'
 end
